81f09c39f2341cad20c690e43b632410e17393f2
sultan <aidaraliuulu@gmail.com>
2019-03-02 11:21:00 +0600

    Создал новый проект. Внутри создал папку src, внутри src создал папку components, containers, store.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import {createStore, applyMiddleware, combineReducers, compose} from 'redux';
+import {Provider} from 'react-redux'
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from "react-router-dom";
+import mainReducer from './store/reducers/root-reducer';
+import thunkMiddleWare from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+import App from "./App";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+const rootReducer = combineReducers({
+    mainReducer: mainReducer
+});
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(thunkMiddleWare)
+);
+
+const app = (
+   <Provider store={store}>
+       <BrowserRouter>
+           <App/>
+       </BrowserRouter>
+   </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
+
 serviceWorker.unregister();
